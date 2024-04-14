68c7cd709cb7f1153b9584f0b8f2d2ccefeb3b26
Max7414 <90671891+Max7414@users.noreply.github.com>
2024-04-14 20:13:48 +0800

    upload ver

--- src/HRSystem/ManagerStaff.java
+++ src/HRSystem/ManagerStaff.java
@@ -1,7 +1,6 @@
 package HRSystem;
 
 public class ManagerStaff extends GeneralStaff {
-	private boolean isManager;
 	ManagerStaff(String name, String password, int departmentChoice, boolean isManager, String workNumber)
 	{
 		super(name, password , departmentChoice, isManager, workNumber);
